3e04f24428c8984e68cd401175a9caaa014b69d4
BaronMsan <lisavandergoes@gmail.com.nl>
2019-01-21 10:06:54 +0100

    Added project

---
+++ Application/app/src/main/java/com/baronmusic/lasa/ksj_launcher/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.baronmusic.lasa.ksj_launcher
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
